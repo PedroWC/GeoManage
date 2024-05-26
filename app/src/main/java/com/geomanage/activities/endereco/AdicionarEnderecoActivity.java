c0aab4503779d308897d7e6af4ad59176396668b
Pedro Carvalhaes <pedrohwc1998@gmail.com>
2024-05-26 11:55:36 -0400

    - InformacoesEndereco style and class deleted

--- app/src/main/java/com/geomanage/activities/endereco/AdicionarEnderecoActivity.java
+++ app/src/main/java/com/geomanage/activities/endereco/AdicionarEnderecoActivity.java
@@ -1,15 +1,15 @@
 package com.geomanage.activities.endereco;
 
+import android.content.ActivityNotFoundException;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ImageButton;
 import android.widget.Spinner;
 import android.widget.Toast;
 
-import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.AppCompatButton;
@@ -18,25 +18,17 @@ import com.geomanage.R;
 import com.geomanage.database.AppDatabase;
 import com.geomanage.entities.Cidade;
 import com.geomanage.entities.Endereco;
-import com.google.android.gms.maps.CameraUpdateFactory;
-import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.material.textfield.TextInputEditText;
 
 import java.util.List;
 import java.util.Objects;
 
-public class AdicionarEnderecoActivity extends AppCompatActivity implements OnMapReadyCallback {
+public class AdicionarEnderecoActivity extends AppCompatActivity {
 
     private static final String TAG = "AdicionarEnderecoActivity";
-    private SupportMapFragment mapFragment;
-    private GoogleMap googleMap;
     private TextInputEditText latitude, longitude, titulo;
     private Spinner spinnerCidades;
-    private Double vlr_latitude = -20.50232, vlr_longitude = -54.61349; // Localização padrão: FACOM
+    private String vlr_latitude = "-20.50232", vlr_longitude = "-54.61349"; // Localização padrão: FACOM
     private String vlr_titulo = "FACOM UFMS";
 
     @Override
@@ -57,42 +49,23 @@ public class AdicionarEnderecoActivity extends AppCompatActivity implements OnMa
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinnerCidades.setAdapter(adapter);
 
-        // Atualizar mapa
-        mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map_endereco);
-        if (mapFragment != null) {
-            mapFragment.getMapAsync(this);
-        } else {
-            Log.e(TAG, "Map Fragment is null");
-        }
-
         // Definir ação do botão que atualiza mapa
         AppCompatButton btnBuscarEndereco = findViewById(R.id.btn_buscar_endereco);
-        btnBuscarEndereco.setOnClickListener(v -> {
-            try {
-                vlr_latitude = Double.parseDouble(Objects.requireNonNull(latitude.getText()).toString());
-                vlr_longitude = Double.parseDouble(Objects.requireNonNull(longitude.getText()).toString());
-                vlr_titulo = Objects.requireNonNull(titulo.getText()).toString();
-                Log.d(TAG, "Buscar Endereço: Latitude: " + vlr_latitude + ", Longitude: " + vlr_longitude + ", Título: " + vlr_titulo);
-                mapFragment.getMapAsync(AdicionarEnderecoActivity.this);
-            } catch (NumberFormatException e) {
-                Log.e(TAG, "Erro ao converter coordenadas", e);
-            }
-        });
+        btnBuscarEndereco.setOnClickListener(v -> { buscarEndereco(); });
 
         // Definir ação do botão que salva o endereço
         AppCompatButton btnSalvarEndereco = findViewById(R.id.btn_salvar_endereco);
         btnSalvarEndereco.setOnClickListener(v -> {
-            vlr_latitude = Double.parseDouble(Objects.requireNonNull(latitude.getText()).toString());
-            vlr_longitude = Double.parseDouble(Objects.requireNonNull(longitude.getText()).toString());
+            vlr_latitude = Objects.requireNonNull(latitude.getText()).toString();
+            vlr_longitude = Objects.requireNonNull(longitude.getText()).toString();
             vlr_titulo = Objects.requireNonNull(titulo.getText()).toString();
             Cidade cidadeSelecionada = (Cidade) spinnerCidades.getSelectedItem();
             int cidadeId = cidadeSelecionada.getCidadeId();
             Endereco endereco = new Endereco();
             endereco.setCidadeId(cidadeId);
             endereco.setDescricao(vlr_titulo);
-            endereco.setLatitude(vlr_latitude);
-            endereco.setLongitude(vlr_longitude);
+            endereco.setLatitude(Double.parseDouble(vlr_latitude));
+            endereco.setLongitude(Double.parseDouble(vlr_longitude));
 
             db.enderecoDao().insert(endereco);
 
@@ -103,37 +76,37 @@ public class AdicionarEnderecoActivity extends AppCompatActivity implements OnMa
         ImageButton btnCancelar = findViewById(R.id.btnCancelar);
         btnCancelar.setOnClickListener(v -> finish());
     }
-
-    @Override
-    public void onMapReady(@NonNull GoogleMap map) {
-        this.googleMap = map;
-        atualizarMapa();
-    }
-
-    public void visualizarInfosEndereco(View view) {
-        try {
-            vlr_latitude = Double.parseDouble(Objects.requireNonNull(latitude.getText()).toString());
-            vlr_longitude = Double.parseDouble(Objects.requireNonNull(longitude.getText()).toString());
-            Intent intent=new Intent(AdicionarEnderecoActivity.this, InformacoesEndereco.class);
-            Bundle bundle=new Bundle();
-            bundle.putDouble("latitude", vlr_latitude);
-            bundle.putDouble("longitude", vlr_longitude);
-            intent.putExtra("bundle", bundle);
-            startActivity(intent);
-        }catch (Exception ignored) {
+    private void buscarEndereco() {
+        vlr_latitude = (Objects.requireNonNull(latitude.getText()).toString());
+        vlr_longitude = (Objects.requireNonNull(longitude.getText()).toString());
+        vlr_titulo = Objects.requireNonNull(titulo.getText()).toString();
+        Log.d(TAG, "Buscar Endereço: Latitude: " + vlr_latitude + ", Longitude: " + vlr_longitude + ", Título: " + vlr_titulo);
+        if (vlr_latitude.isEmpty() || vlr_longitude.isEmpty()){
+            Toast.makeText(this, "Latitude e longitude obrigatórias", Toast.LENGTH_SHORT).show();
+            return;
         }
-    }
+        double lat, longi;
+        try {
+            lat = Double.parseDouble(vlr_latitude);
+            longi = Double.parseDouble(vlr_longitude);
+        }catch (NumberFormatException e){
+            Toast.makeText(this, "Latitude e longitude devem ser válidos", Toast.LENGTH_SHORT).show();
+            return;
+        }
 
-    private void atualizarMapa() {
-        if (googleMap != null) {
-            LatLng latLng = new LatLng(vlr_latitude, vlr_longitude);
-            googleMap.addMarker(new MarkerOptions().position(latLng).title(vlr_titulo));
-            googleMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
-            googleMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
-            googleMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
-            googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 15));
-        } else {
-            Log.e(TAG, "GoogleMap não está pronto");
+        int zoomLevel = 15;
+        String formatedUri = String.format("geo:%s,%s?q=%s,%s&z=%d", lat, longi, lat, longi, zoomLevel);
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(formatedUri));
+        intent.setPackage("com.google.android.apps.maps");
+        try {
+            startActivity(intent);
+        }catch (ActivityNotFoundException e){
+            Intent unrestrictedIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(formatedUri));
+            try {
+                startActivity(unrestrictedIntent);
+            }catch (ActivityNotFoundException innerE){
+                Toast.makeText(this, "Nenhuma aplicação encontrada", Toast.LENGTH_SHORT).show();
+            }
         }
     }
 }
