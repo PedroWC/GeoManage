604508b646c5d531f6938ac3ed2ef7fd398d47f2
RobertoFORTs <bobfortesneto@gmail.com>
2024-05-26 11:03:52 -0400

    feat: open in maps

--- app/src/main/java/com/geomanage/activities/endereco/AdicionarEnderecoActivity.java
+++ app/src/main/java/com/geomanage/activities/endereco/AdicionarEnderecoActivity.java
@@ -1,10 +1,13 @@
 package com.geomanage.activities.endereco;
 
+import android.content.ActivityNotFoundException;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -22,13 +25,13 @@ import com.google.android.material.textfield.TextInputEditText;
 
 import java.util.Objects;
 
-public class AdicionarEnderecoActivity extends AppCompatActivity implements OnMapReadyCallback {
+public class AdicionarEnderecoActivity extends AppCompatActivity {
 
     private static final String TAG = "AdicionarEnderecoActivity";
 
     private GoogleMap googleMap;
     private TextInputEditText latitude, longitude, titulo;
-    private Double vlr_latitude = -20.50232, vlr_longitude = -54.61349; // Localização padrão: FACOM
+    private String vlr_latitude = "-20.50232", vlr_longitude = "-54.61349"; // Localização padrão: FACOM
     private String vlr_titulo = "Localização Padrão";
 
     @Override
@@ -39,15 +42,6 @@ public class AdicionarEnderecoActivity extends AppCompatActivity implements OnMa
         latitude = findViewById(R.id.edt_latitudeEndereco);
         longitude = findViewById(R.id.edt_longitudeEndereco);
         titulo = findViewById(R.id.edt_tituloEndereco);
-        System.out.println("ANTES DO SUPPORT MANAGER");
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map_endereco);
-        if (mapFragment != null) {
-            mapFragment.getMapAsync(this);
-        } else {
-            Log.e(TAG, "Map Fragment is null");
-        }
-
         AppCompatButton btnBuscarEndereco = findViewById(R.id.btn_buscar_endereco);
         btnBuscarEndereco.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -60,34 +54,37 @@ public class AdicionarEnderecoActivity extends AppCompatActivity implements OnMa
         btnCancelar.setOnClickListener(v -> finish());
     }
 
-    @Override
-    public void onMapReady(@NonNull GoogleMap map) {
-        this.googleMap = map;
-        atualizarMapa();
-    }
-
     private void buscarEndereco() {
-        try {
-            vlr_latitude = Double.parseDouble(Objects.requireNonNull(latitude.getText()).toString());
-            vlr_longitude = Double.parseDouble(Objects.requireNonNull(longitude.getText()).toString());
-            vlr_titulo = Objects.requireNonNull(titulo.getText()).toString();
-            Log.d(TAG, "Buscar Endereço: Latitude: " + vlr_latitude + ", Longitude: " + vlr_longitude + ", Título: " + vlr_titulo);
-            atualizarMapa();
-        } catch (NumberFormatException e) {
-            Log.e(TAG, "Erro ao converter coordenadas", e);
+        vlr_latitude = (Objects.requireNonNull(latitude.getText()).toString());
+        vlr_longitude = (Objects.requireNonNull(longitude.getText()).toString());
+        vlr_titulo = Objects.requireNonNull(titulo.getText()).toString();
+        Log.d(TAG, "Buscar Endereço: Latitude: " + vlr_latitude + ", Longitude: " + vlr_longitude + ", Título: " + vlr_titulo);
+        if (vlr_latitude.isEmpty() || vlr_longitude.isEmpty()){
+            Toast.makeText(this, "Latitude e longitude obrigatórias", Toast.LENGTH_SHORT).show();
+            return;
         }
-    }
+        double lat, longi;
+        try {
+            lat = Double.parseDouble(vlr_latitude);
+            longi = Double.parseDouble(vlr_longitude);
+        }catch (NumberFormatException e){
+            Toast.makeText(this, "Latitude e longitude devem ser válidos", Toast.LENGTH_SHORT).show();
+            return;
+        }
 
-    private void atualizarMapa() {
-        if (googleMap != null) {
-            LatLng latLng = new LatLng(vlr_latitude, vlr_longitude);
-            googleMap.clear();
-            googleMap.addMarker(new MarkerOptions().position(latLng).title(vlr_titulo));
-            googleMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
-            googleMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
-            googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 15));
-        } else {
-            Log.e(TAG, "GoogleMap não está pronto");
+        int zoomLevel = 15;
+        String formatedUri = String.format("geo:%s,%s?q=%s,%s&z=%d", lat, longi, lat, longi, zoomLevel);
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(formatedUri));
+        intent.setPackage("com.google.android.apps.maps");
+        try {
+            startActivity(intent);
+        }catch (ActivityNotFoundException e){
+            Intent unrestrictedIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(formatedUri));
+            try {
+                startActivity(unrestrictedIntent);
+            }catch (ActivityNotFoundException innerE){
+                Toast.makeText(this, "Nenhuma aplicação encontrada", Toast.LENGTH_SHORT).show();
+            }
         }
     }
 }
